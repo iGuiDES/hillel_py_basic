98b981f9fe9f270cd2133c50b394d08da5ad540b
iGuiDES <iosiguides@gmail.com>
2023-10-06 18:23:32 +0300

    Fixed bug task 4

--- hw_03/task_4.py
+++ hw_03/task_4.py
@@ -1,6 +1,6 @@
 year = int(input('Введіть рік: '))
 
-if year < 1900 or year >= 1_000_000:
+if year < 1900 or year < 1_000_000:
     print('Рік', year, 'не відповідає вимогам!')
 else:
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
