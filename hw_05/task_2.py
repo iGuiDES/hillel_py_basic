d5960af248fe1000851177ff492ab3fc99a69cb4
iGuiDES <iosiguides@gmail.com>
2023-10-16 18:50:16 +0300

    Fixed bug in task 2

--- hw_05/task_2.py
+++ hw_05/task_2.py
@@ -3,7 +3,7 @@ N = int(input("Введіть число N: "))
 numbers = []
 
 for i in range(1, N + 1):
-    square = i ** i
+    square = i ** 2
 
     if str(square)[-len(str(i)):] == str(i):
         numbers.append(i)
