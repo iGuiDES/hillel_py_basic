a41fca5b1d5d1c82078106fb52b504f482d1dee8
iGuiDES <iosiguides@gmail.com>
2023-10-18 12:15:27 +0300

    Added calculate the width by the maximum number of numbers

--- hw_06/task_2.py
+++ hw_06/task_2.py
@@ -1,5 +1,6 @@
 user_number = int(input("Input number: "))
+with_n = len(str(user_number))
 
 for i in range(user_number):
     line = '1' + '0' * i
-    print(f"{i} {line.rjust(user_number + 1)}")
+    print(f"{i:{with_n}} {line.rjust(user_number + 1)}")
